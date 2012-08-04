9db7e692e7c3cc3765fdf7b952958b4d6dec3d9b
aoisensi <aoisensi@satoru-PC.(none)>
2012-08-04 18:09:14 +0900

    this is a test

--- src/info/aoisensi/KFSurvival/KFSurvival.java
+++ src/info/aoisensi/KFSurvival/KFSurvival.java
@@ -1,5 +1,18 @@
 package info.aoisensi.KFSurvival;
 
-public class KFSurvival {
+import java.util.logging.Logger;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class KFSurvival extends JavaPlugin {
+	Logger log;
+	 
+	public void onEnable(){
+		log = this.getLogger();
+		log.info("Your plugin has been enabled!");
+	}
+ 
+	public void onDisable(){
+		log.info("Your plugin has been disabled.");
+	}
 }
